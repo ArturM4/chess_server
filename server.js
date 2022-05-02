bcc27298e4041cf5457cd05f8403896bbc2c0b61
arturmh <52570589+ArturM4@users.noreply.github.com>
2022-05-02 13:42:59 +0200

    init server and game creation

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const socketio = require("socket.io");
+var path = require('path');
+
+const io = socketio(server)
+
+let currentGames = {};
+
+io.on("connection", socket => {
+
+  socket.on("createGame", (id) => {
+    let game = {
+      gameId: id,
+      playerIds: [socket.id],
+      creatorIsWhite: true
+    }
+    currentGames[id] = game
+    console.log(game)
+  })
+
+  socket.on("joinGame", (id) => {
+    if (currentGames[id]?.playerIds) {
+      if (currentGames[id].playerIds.length < 2 && !currentGames[id].playerIds.includes(socket.id)) {
+        currentGames[id].playerIds.push(socket.id)
+        if (currentGames[id].playerIds.length === 2) {
+          io.sockets.to(currentGames[id].playerIds[0]).emit("gameInit", currentGames[id].creatorIsWhite)
+          io.sockets.to(currentGames[id].playerIds[1]).emit("gameInit", !currentGames[id].creatorIsWhite)
+        }
+      } else if (currentGames[id].playerIds.length === 2 && currentGames[id].playerIds.includes(socket.id)) {
+        if (socket.id === currentGames[id].playerIds[0])
+          socket.emit("gameInit", currentGames[id].creatorIsWhite)
+        else
+          socket.emit("gameInit", !currentGames[id].creatorIsWhite)
+      }
+    }
+    console.log(currentGames[id])
+  })
+
+})
+
+const PORT = process.env.PORT || 3001
+server.listen(PORT, () => console.log("Servidor inicialitzat"))
