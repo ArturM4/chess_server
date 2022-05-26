1d559afbfa016c821a1675ec23f0c796520ca732
arturmh <52570589+ArturM4@users.noreply.github.com>
2022-05-26 15:38:51 +0200

    cancel search, different time modes

--- server.js
+++ server.js
@@ -16,22 +16,38 @@ app.get('*', (req, res) => {
 const io = socketio(server)
 
 let currentGames = {}
-let searchingGames = []
+let searchingGames = {
+  bullet: [],
+  blitz: [],
+  rapid: [],
+  classic: []
+}
 
-function createGame(ids) {
+function createGame(ids, mode) {
 
   let id = uuid()
   let game = {
     gameId: id,
     playerIds: ids,
-    creatorIsWhite: true
+    creatorIsWhite: true,
+    mode
   }
   currentGames[id] = game
   io.sockets.to(currentGames[id].playerIds[0]).emit("gameInit", id)
   io.sockets.to(currentGames[id].playerIds[1]).emit("gameInit", id)
 }
 
+function cancelSearch(id) {
+  Object.keys(searchingGames).forEach(function (key) {
+    var index = searchingGames[key].indexOf(id);
+    if (index > -1) {
+      searchingGames[key].splice(index, 1);
+    }
+  });
+}
+
 io.on("connection", socket => {
+
 
   socket.on("createGame", (id) => {
     let game = {
@@ -42,13 +58,13 @@ io.on("connection", socket => {
     currentGames[id] = game
   })
 
-  socket.on("searchGame", () => {
+  socket.on("searchGame", (mode) => {
 
-    if (searchingGames.length === 0)
-      searchingGames.push(socket.id)
+    if (searchingGames[mode].length === 0)
+      searchingGames[mode].push(socket.id)
     else {
-      playerSearching = searchingGames.pop()
-      createGame([playerSearching, socket.id])
+      playerSearching = searchingGames[mode].pop()
+      createGame([playerSearching, socket.id], mode)
     }
   })
 
@@ -57,26 +73,42 @@ io.on("connection", socket => {
       if (currentGames[id].playerIds.length < 2 && !currentGames[id].playerIds.includes(socket.id)) {
         currentGames[id].playerIds.push(socket.id)
         if (currentGames[id].playerIds.length === 2) {
-          io.sockets.to(currentGames[id].playerIds[0]).emit("gameInit", currentGames[id].creatorIsWhite)
-          io.sockets.to(currentGames[id].playerIds[1]).emit("gameInit", !currentGames[id].creatorIsWhite)
+          io.sockets.to(currentGames[id].playerIds[0]).emit("gameInit", currentGames[id].creatorIsWhite, currentGames[id].mode)
+          io.sockets.to(currentGames[id].playerIds[1]).emit("gameInit", !currentGames[id].creatorIsWhite, currentGames[id].mode)
         }
       } else if (currentGames[id].playerIds.length === 2 && currentGames[id].playerIds.includes(socket.id)) {
         if (socket.id === currentGames[id].playerIds[0])
-          socket.emit("gameInit", currentGames[id].creatorIsWhite)
+          socket.emit("gameInit", currentGames[id].creatorIsWhite, currentGames[id].mode)
         else
-          socket.emit("gameInit", !currentGames[id].creatorIsWhite)
+          socket.emit("gameInit", !currentGames[id].creatorIsWhite, currentGames[id].mode)
       }
     }
   })
 
-  socket.on("doMove", (id, move) => {
+  socket.on("doMove", (id, move, time) => {
     if (currentGames[id] && currentGames[id].playerIds.includes(socket.id)) {
       otherPlayerId = currentGames[id].playerIds.find(playerId => playerId !== socket.id)
-      io.sockets.to(otherPlayerId).emit("moveDone", move)
+      io.sockets.to(otherPlayerId).emit("moveDone", move, time)
     }
   })
 
+  socket.on("receivedMove", (id) => {
+    if (currentGames[id] && currentGames[id].playerIds.includes(socket.id)) {
+      otherPlayerId = currentGames[id].playerIds.find(playerId => playerId !== socket.id)
+      io.sockets.to(otherPlayerId).emit("oppponentReceivedMove")
+    }
+  })
+
+  socket.on("cancelSearch", () => {
+    cancelSearch(socket.id)
+  })
+
+  socket.on("disconnect", () => {
+    cancelSearch(socket.id)
+  })
+
 })
+
 
 const PORT = process.env.PORT || 3001
 server.listen(PORT, () => console.log("Servidor inicialitzat"))
